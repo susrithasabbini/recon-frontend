87631b4f097fbe980aa91b15e5494183259352a3
Susritha Sabbini <susrithasabbini@gmail.com>
2025-05-19 19:31:24 +0530

    feat: added account creation page

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -8,7 +8,7 @@ export default function DefaultLayout({
   return (
     <div className="relative flex flex-col h-screen">
       <Navbar />
-      <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
+      <main className="container mx-auto px-20 flex-grow pt-16">
         {children}
       </main>
     </div>
